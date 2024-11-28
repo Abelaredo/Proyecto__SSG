905455791c8283163c8e4ccb47eafe3aaf063c30
Abelaredo <abelsobrecuevasantos@gmail.com>
2024-11-28 08:33:05 +0100

    210

--- generate_graph.py
+++ generate_graph.py
@@ -1,33 +1,44 @@
+
 import matplotlib.pyplot as plt
 import requests
 
-REPO = "Abelaredo/Proyecto__SSG"
+REPO = "user/repo"
 API_URL = f"https://api.github.com/repos/{REPO}/contributors"
 
 response = requests.get(API_URL)
 data = response.json()
 
-data = [user for user in data if user['login'] != 'actions-user']
-
 contributors = [user['login'] for user in data]
 contributions = [user['contributions'] for user in data]
-
-# Obtener el rango de contribuciones
-min_contrib = min(contributions)
-max_contrib = max(contributions)
-
-# Crear una lista con todos los números del rango
-all_ticks = list(range(min_contrib, max_contrib + 1))
 
 plt.barh(contributors, contributions, color='skyblue')
 plt.xlabel('Contributions')
 plt.ylabel('Users')
 plt.title('GitHub Contributions')
 
-# Establecer los ticks en todos los números
-plt.xticks(all_ticks)
-
-# Configurar para que solo se muestren las etiquetas de los múltiplos de 5
-plt.gca().xaxis.set_major_locator(plt.MultipleLocator(5))
+# Set ticks on X-axis to show multiples of 5
+max_contributions = max(contributions)
+plt.xticks(range(0, max_contributions + 1, 5))  # Multiples of 5 up to the max contributions
 
 plt.savefig('contributions.png')
+
+'''import matplotlib.pyplot as plt
+import requests
+
+REPO = "user/repo"
+API_URL = f"https://api.github.com/repos/{REPO}/contributors"
+
+response = requests.get(API_URL)
+data = response.json()
+
+contributors = [user['login'] for user in data]
+contributions = [user['contributions'] for user in data]
+
+plt.barh(contributors, contributions, color='skyblue')
+plt.xlabel('Contributions')
+plt.ylabel('Users')
+plt.title('GitHub Contributions')
+
+plt.savefig('contributions.png')'''
+
+
